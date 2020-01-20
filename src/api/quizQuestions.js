2ba7905463304efb121ffa826d76c9bf9d806b91
Bas van Roemburg <basbas@gmail.com>
2020-01-20 12:01:09 +0100

    Fixed score logic, questions and adding score to hidden ActiveCampaign field after form rendering

--- src/api/quizQuestions.js
+++ src/api/quizQuestions.js
@@ -1,283 +1,265 @@
 var quizQuestions = [
   {
-      question: "Ik doe werk waar ik goed in ben en volledig in mijn kracht sta  ",
-      answers: [
-          {
-              type: '1',
-              content: "Helemaal oneens"
-          },
-          {
-              type: '2',
-              content: "Oneens"
-          },
-          {
-              type: '3',
-              content: "Eens"
-          },
-          {
-            type: '4',
-            content: "Helemaal eens"
-        }
-      ]
+    question:
+      "Ik doe werk waar ik goed in ben en volledig in mijn kracht sta  ",
+    answers: [
+      {
+        type: "1",
+        content: "Helemaal oneens"
+      },
+      {
+        type: "2",
+        content: "Oneens"
+      },
+      {
+        type: "3",
+        content: "Eens"
+      },
+      {
+        type: "4",
+        content: "Helemaal eens"
+      }
+    ]
   },
   {
-      question: "Ik voel me thuis in de organisatie waar ik werk",
-      answers: [
-        {
-            type: '1',
-            content: "Helemaal oneens"
-        },
-        {
-            type: '2',
-            content: "Oneens"
-        },
-        {
-            type: '3',
-            content: "Eens"
-        },
-        {
-          type: '4',
-          content: "Helemaal eens"
-        }
+    question: "Ik voel me thuis in de organisatie waar ik werk",
+    answers: [
+      {
+        type: "1",
+        content: "Helemaal oneens"
+      },
+      {
+        type: "2",
+        content: "Oneens"
+      },
+      {
+        type: "3",
+        content: "Eens"
+      },
+      {
+        type: "4",
+        content: "Helemaal eens"
+      }
     ]
-     
   },
   {
     question: "Ik werk aan een belangrijk doel waar ik volledig achtersta",
     answers: [
       {
-          type: '1',
-          content: "Helemaal oneens"
+        type: "1",
+        content: "Helemaal oneens"
       },
       {
-          type: '2',
-          content: "Oneens"
+        type: "2",
+        content: "Oneens"
       },
       {
-          type: '3',
-          content: "Eens"
+        type: "3",
+        content: "Eens"
       },
       {
-        type: '4',
+        type: "4",
         content: "Helemaal eens"
       }
     ]
-   
-},
-{
-    question: "Ik zie dat het werk wat ik doe relevant is en waarde toevoegt voor het bedrijf ",
+  },
+  {
+    question:
+      "Ik zie dat het werk wat ik doe relevant is en waarde toevoegt voor de organisatie ",
     answers: [
       {
-          type: '1',
-          content: "Helemaal oneens"
+        type: "1",
+        content: "Helemaal oneens"
       },
       {
-          type: '2',
-          content: "Oneens"
+        type: "2",
+        content: "Oneens"
       },
       {
-          type: '3',
-          content: "Eens"
+        type: "3",
+        content: "Eens"
       },
       {
-        type: '4',
+        type: "4",
         content: "Helemaal eens"
       }
     ]
-   
-},
-{
-    question: "Ik word door mijn collega’s gewaardeerd om de bijdrage die ik lever ",
+  },
+  {
+    question:
+      "Ik word door mijn collega’s gewaardeerd om de bijdrage die ik lever ",
     answers: [
       {
-          type: '1',
-          content: "Helemaal oneens"
+        type: "1",
+        content: "Helemaal oneens"
       },
       {
-          type: '2',
-          content: "Oneens"
+        type: "2",
+        content: "Oneens"
       },
       {
-          type: '3',
-          content: "Eens"
+        type: "3",
+        content: "Eens"
       },
       {
-        type: '4',
+        type: "4",
         content: "Helemaal eens"
+      }
+    ]
+  },
+  {
+    question:
+      "Ik ontwikkel mezelf in m’n werk en heb het gevoel dat ik telkens beter word in mijn vak",
+    answers: [
+      {
+        type: "1",
+        content: "Helemaal oneens"
       },
-
-
-     ]
-  
-    },
-    {
-        question: "Ik word door mijn collega’s gewaardeerd om de bijdrage die ik lever ",
-        answers: [
-          {
-              type: '1',
-              content: "Helemaal oneens"
-          },
-          {
-              type: '2',
-              content: "Oneens"
-          },
-          {
-              type: '3',
-              content: "Eens"
-          },
-          {
-            type: '4',
-            content: "Helemaal eens"
-          },
-    
-    
-         ]
-      
-        },
-        {
-            question: "Ik word door mijn collega’s gewaardeerd om de bijdrage die ik lever ",
-            answers: [
-              {
-                  type: '1',
-                  content: "Helemaal oneens"
-              },
-              {
-                  type: '2',
-                  content: "Oneens"
-              },
-              {
-                  type: '3',
-                  content: "Eens"
-              },
-              {
-                type: '4',
-                content: "Helemaal eens"
-              },
-        
-        
-             ]
-          
-            },
-            {
-                question: "Ik word door mijn collega’s gewaardeerd om de bijdrage die ik lever ",
-                answers: [
-                  {
-                      type: '1',
-                      content: "Helemaal oneens"
-                  },
-                  {
-                      type: '2',
-                      content: "Oneens"
-                  },
-                  {
-                      type: '3',
-                      content: "Eens"
-                  },
-                  {
-                    type: '4',
-                    content: "Helemaal eens"
-                  },
-            
-            
-                 ]
-              
-                },            {
-                    question: "Ik word door mijn collega’s gewaardeerd om de bijdrage die ik lever ",
-                    answers: [
-                      {
-                          type: '1',
-                          content: "Helemaal oneens"
-                      },
-                      {
-                          type: '2',
-                          content: "Oneens"
-                      },
-                      {
-                          type: '3',
-                          content: "Eens"
-                      },
-                      {
-                        type: '4',
-                        content: "Helemaal eens"
-                      },
-                
-                
-                     ]
-                  
-                    }
-                    ,            {
-                        question: "Ik word door mijn collega’s gewaardeerd om de bijdrage die ik lever ",
-                        answers: [
-                          {
-                              type: '1',
-                              content: "Helemaal oneens"
-                          },
-                          {
-                              type: '2',
-                              content: "Oneens"
-                          },
-                          {
-                              type: '3',
-                              content: "Eens"
-                          },
-                          {
-                            type: '4',
-                            content: "Helemaal eens"
-                          },
-                    
-                    
-                         ]
-                      
-                        },
-                        {
-                            question: "Ik word door mijn collega’s gewaardeerd om de bijdrage die ik lever ",
-                            answers: [
-                              {
-                                  type: '1',
-                                  content: "Helemaal oneens"
-                              },
-                              {
-                                  type: '2',
-                                  content: "Oneens"
-                              },
-                              {
-                                  type: '3',
-                                  content: "Eens"
-                              },
-                              {
-                                type: '4',
-                                content: "Helemaal eens"
-                              },
-                        
-                        
-                             ]
-                          
-                            }
-                            ,            {
-                                question: "Ik word door mijn collega’s gewaardeerd om de bijdrage die ik lever ",
-                                answers: [
-                                  {
-                                      type: '1',
-                                      content: "Helemaal oneens"
-                                  },
-                                  {
-                                      type: '2',
-                                      content: "Oneens"
-                                  },
-                                  {
-                                      type: '3',
-                                      content: "Eens"
-                                  },
-                                  {
-                                    type: '4',
-                                    content: "Helemaal eens"
-                                  },
-                            
-                            
-                                 ]
-                              
-                                }
-                                
-]
+      {
+        type: "2",
+        content: "Oneens"
+      },
+      {
+        type: "3",
+        content: "Eens"
+      },
+      {
+        type: "4",
+        content: "Helemaal eens"
+      }
+    ]
+  },
+  {
+    question:
+      "Ik vind inhoudelijk het werk dat ik doe leuk en voldoende uitdagend",
+    answers: [
+      {
+        type: "1",
+        content: "Helemaal oneens"
+      },
+      {
+        type: "2",
+        content: "Oneens"
+      },
+      {
+        type: "3",
+        content: "Eens"
+      },
+      {
+        type: "4",
+        content: "Helemaal eens"
+      }
+    ]
+  },
+  {
+    question:
+      "Ik kan me optrekken aan de mensen met wie ik samenwerk. Er zitten echt mensen in mijn team van wie ik kan leren.",
+    answers: [
+      {
+        type: "1",
+        content: "Helemaal oneens"
+      },
+      {
+        type: "2",
+        content: "Oneens"
+      },
+      {
+        type: "3",
+        content: "Eens"
+      },
+      {
+        type: "4",
+        content: "Helemaal eens"
+      }
+    ]
+  },
+  {
+    question:
+      "Er heerst een goede teamsfeer waarbij we elkaar helpen als het nodig is",
+    answers: [
+      {
+        type: "1",
+        content: "Helemaal oneens"
+      },
+      {
+        type: "2",
+        content: "Oneens"
+      },
+      {
+        type: "3",
+        content: "Eens"
+      },
+      {
+        type: "4",
+        content: "Helemaal eens"
+      }
+    ]
+  },
+  {
+    question: "Ik heb de ruimte om eigen invulling te geven aan mijn rol.",
+    answers: [
+      {
+        type: "1",
+        content: "Helemaal oneens"
+      },
+      {
+        type: "2",
+        content: "Oneens"
+      },
+      {
+        type: "3",
+        content: "Eens"
+      },
+      {
+        type: "4",
+        content: "Helemaal eens"
+      }
+    ]
+  },
+  {
+    question:
+      "Ik heb het gevoel dat ik gehoord word. Dat er naar mij geluisterd wordt als er belangrijke besluiten genomen worden.",
+    answers: [
+      {
+        type: "1",
+        content: "Helemaal oneens"
+      },
+      {
+        type: "2",
+        content: "Oneens"
+      },
+      {
+        type: "3",
+        content: "Eens"
+      },
+      {
+        type: "4",
+        content: "Helemaal eens"
+      }
+    ]
+  },
+  {
+    question:
+      "Ik ervaar een gezonde balans tussen mijn privéleven en mijn werk",
+    answers: [
+      {
+        type: "1",
+        content: "Helemaal oneens"
+      },
+      {
+        type: "2",
+        content: "Oneens"
+      },
+      {
+        type: "3",
+        content: "Eens"
+      },
+      {
+        type: "4",
+        content: "Helemaal eens"
+      }
+    ]
+  }
+];
 
 export default quizQuestions;
